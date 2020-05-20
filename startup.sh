58e8472cf26e71575026882c475c4902930c904d
greinet <57115527+greinet@users.noreply.github.com>
2020-05-20 09:42:15 +0200

    Moved 128 tick settings to server.cfg

--- startup.sh
+++ startup.sh
@@ -10,6 +10,15 @@ if [ ! -f "/steam/pluginmarker" ]; then
   echo "Installing plugins"
   cd /steam/plugins
   
+  #server.cfg
+  mkdir cfg
+  
+  echo "sv_maxrate 0
+sv_minrate 80000
+sv_mincmdrate 128
+sv_maxupdaterate 128
+sv_minupdaterate 128" >> cfg/server.cfg
+  
   #SM und MM
   echo "Installing Sourcemod and Metamod"
   curl -sqL "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git971-linux.tar.gz"  | tar xz -C /steam/plugins/
@@ -32,4 +41,4 @@ if [ ! -f "/steam/pluginmarker" ]; then
 fi
 
 # Start csgo
-/steam/csgo/srcds_run -sv_maxrate 0 -sv_minrate 80000 -sv_mincmdrate 128 -sv_maxupdaterate 128 -sv_minupdaterate 128 -game csgo -console -usercon -strictportbind -port 27015 +clientport 27005 +tv_port 27020 -tickrate 128 +log on +game_type 0 +game_mode 0 +mapgroup mg_bomb +map de_dust -authkey -unsecure -insecure +rcon_password rconpw123 +sv_setsteamaccount $GSLT -net_port_try 1 +hostname hostname
+/steam/csgo/srcds_run -game csgo -console -usercon -strictportbind -port 27015 +clientport 27005 +tv_port 27020 -tickrate 128 +log on +game_type 0 +game_mode 0 +mapgroup mg_bomb +map de_dust -authkey -unsecure -insecure +rcon_password rconpw123 +sv_setsteamaccount $GSLT -net_port_try 1 +hostname hostname
